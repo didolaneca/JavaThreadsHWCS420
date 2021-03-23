1d19954d7b6d27754e6387a5db38b9da97d4db96
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-03-23 13:49:14 -0500

    Car class for Task 2 completed

--- Threads/task2/src/Car.java
+++ Threads/task2/src/Car.java
@@ -6,4 +6,31 @@ public class Car {
 
  /* TODO 
 Exact same methods and constructor as the Car in task 1*/
+
+    public Car(String name,double miles){
+        this.miles = miles;
+        odometer = 0;
+        this.name = name;
+        this.maxMiles = miles;
+    }
+
+    public void move(double miles){
+        double ml = this.miles - miles;
+        if(ml >= 0){
+            this.miles = ml;
+            this.odometer += miles;
+        } else {
+            this.odometer += this.miles;
+            this.miles = 0;
+        }
+
+    }
+
+    public void charge(){
+        this.miles = maxMiles;
+    }
+
+    public String toString(){
+        return name+": odo:"+odometer+" - miles left:"+miles;
+    }
 }
