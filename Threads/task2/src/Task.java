49b5bc62ba735d40af2da2ab2a60667836516c31
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-03-23 15:41:16 -0500

    Task class for Task 2 completed and passing the tests

--- Threads/task2/src/Task.java
+++ Threads/task2/src/Task.java
@@ -1,24 +1,31 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class Task {
     /* TODO
 Declare a static ExecutorService named service */
+    static ExecutorService service;
     public static void main(String[] args){
         Car c1 = new Car("Kona",250);
         Car c2 = new Car("Ioniq",220);
 
-        Runnable racer1 = /* TODO 
+        Runnable racer1 = new Racer(c1); /* TODO
 Create a Runnable with c1*/
-        Runnable racer2 = /* TODO 
+        Runnable racer2 = new Racer(c2); /* TODO
 Create a Runnable with c2*/;
 
         initExecutorService();
 
         /* TODO
 Submit both runnables to the ExecutorService and then request the ExecutorService to shutdown */
-  }
+        Future<?> racerOneResult = service.submit(racer1);
+        Future<?> racerTwoResult = service.submit(racer2);
+    }
 
   /* TODO
 Implement the initExecutorService method. This has to initialize service to a new FixedThreadPool with a minimum of 3 threads */
+    static void initExecutorService(){
+        service = Executors.newFixedThreadPool(3);
+    }
 }
