6d9d761e6725666feed4f5211af2556b3e3a5593
didolaneca@yahoo.com <didolaneca@yahoo.com>
2021-03-31 20:04:25 -0500

    Final commit. Everything working.

--- Threads/task3/src/WebPage.java
+++ Threads/task3/src/WebPage.java
@@ -1,15 +1,27 @@
 /* TODO
 Imports will be filling up as you need them. */
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class WebPage {
     public List<String> dom; // this actually has the html document model.
 
     public WebPage(){
-        dom = /* TODO 
+        dom = new LinkedList<>(); /* TODO
 Initialize this to some kind of List */;
     }
 
     public void addTitle(String title){
+        StringBuffer stringBuffer = new StringBuffer()
+                .append("<h1>")
+                .append(title)
+                .append("</h1>");
+        this.dom.add(stringBuffer.toString());
         /* TODO 
 Add a title to the DOM*/
     }
@@ -17,15 +29,35 @@ Add a title to the DOM*/
     /* TODO 
 implement addParagraph that receives a String and puts it in the dom 
 surrounded by <p> and </p>*/
+    public void addParagraph(String paragraph){
+        StringBuffer stringBuffer = new StringBuffer()
+                .append("<p>")
+                .append(paragraph)
+                .append("</p>");
+        this.dom.add(stringBuffer.toString());
+    }
+
     public String toString(){
         String html = "<html>\n<head>\n</head>\n<body>";
         /* TODO
 Loop through the elemments of the dom and append them to html */
-        return html+"</body>\n</html>";
+        StringBuffer stringBuffer = new StringBuffer(html);
+        for (String el : dom) {
+            stringBuffer.append(el);
+        }
+        return stringBuffer
+                .append("</body>\n</html>")
+                .toString();
     }
 
-    public void save(String filename){
+    public void save(String filename) {
         /* TODO 
 Save a string representation of this webpage to the filename provided.*/
+        File file = new File(filename);
+        try(FileWriter fileWriter = new FileWriter(file)){
+            fileWriter.append(filename);
+        } catch (IOException ex) {
+            System.out.println("Opps : " + ex.getMessage());
+        }
     }
 }
--- Threads/task3/src/WebPageEditor.java
+++ Threads/task3/src/WebPageEditor.java
@@ -1,6 +1,11 @@
 /* TODO 
 Imports. Fill them up as you need them.*/
 
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class WebPageEditor {
     public  String temp; // holds the last webpage saved.
     public  WebPage wp; // the webpage you are working on
@@ -9,28 +14,43 @@ public class WebPageEditor {
     public WebPageEditor(String filename){
 /* TODO
 Initialize wp to an empty webpage, and filename to the value passed. */
+        this.wp = new WebPage();
+        this.filename = filename;
     }
 
     public void start(){
-
-        ScheduledExecutorService service = /* TODO
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();/* TODO
 Using Executors, instantiate a new SingleThreadedScheduledExecutor */
         /* TODO 
 Schedule at a fixed rate a runnable that executes checkSave. if checkSave is true it will print "Saved", This has to be schdule to start in 10 seconds and repeat every 10 seconds.  */
-
+        service.scheduleAtFixedRate(this::checkSave, 10, 10, TimeUnit.SECONDS);
         // use a scanner to read every line the user writes
         Scanner kbd  = new Scanner(System.in);
         boolean finish = false;
         while(!finish){
 /* TODO 
 If the user types "quit" the loop ends. If the input starts with t: you add a title to the webpage. If not, you add a paragraph.*/
+            String userInput = kbd.nextLine();
+            if(userInput.equals("quit") || userInput.equals("q")){
+                finish = true;
+            } else if ("t:".equals(userInput.substring(0, 2))){
+                this.wp.addTitle(userInput.substring(2));
+            } else {
+                this.wp.addParagraph(userInput);
+            }
         }
-        service./* TODO 
+        service.shutdown()/* TODO
 Shutdown the threads NOW*/; // when the loop finishes, threads are stopped
     }
 
     public  boolean checkSave(){
         /* TODO 
 If the text of the wp (String representation) is equal to the last String reperesentation (stored in tmp) returns false. Otherwise, it saves the webpage to the given filenam, tmp updates to the most current string representation of this webpage, and the funtion returns true.*/
+        if (this.wp.toString().equals(this.temp)){
+            return false;
+        } else {
+            this.wp.save(filename);
+            return true;
+        }
     }
 }
